f8629e8de86abb05d8f7eb367bfea22c78c919d2
David Mignot <david.mig@gmail.com>
2015-05-25 13:36:55 +0200

    simplify and split css

--- config.rb
+++ config.rb
@@ -1,7 +1,3 @@
-# Require any additional compass plugins here.
-require "susy"
-require "breakpoint"
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "assets/styles"
